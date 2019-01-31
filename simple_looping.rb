8af2591bb0c6af3e1d79dfbb82923b18fc1989d1
Nikka Hassan <nikka.hassan@gmail.com>
2019-01-31 21:48:01 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -12,8 +12,8 @@ end
 
   
   def times_iterator(number_of_times)
-  # code your solution here using the "times" keyword
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+  
+  puts "Welcome to Flatiron School's Web Development Course!"
   
 end
 
