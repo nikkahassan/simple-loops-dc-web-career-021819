7c00966f62a0efd4d52ef1c46d00f95adaacba7b
Nikka Hassan <nikka.hassan@gmail.com>
2019-01-31 22:39:13 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -27,9 +27,12 @@ def while_iterator(number_of_times)
 end
 
 def until_iterator(number_of_times)
- 
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-
+ phrase = "Welcome to Flatiron School's Web Development Course!"
+counter = 0
+  until counter == number_of_times
+    puts phrase
+    counter += 1
+  end
 end
 
 def for_iterator(number_of_times)
